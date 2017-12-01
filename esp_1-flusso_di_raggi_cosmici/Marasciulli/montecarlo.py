95319101e7969e77555356ab2900868e7b2cc9bd
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2017-12-01 22:41:24 +0100

    Aggiornamento
    Ho potenziato il programma permettendogli di scegliere l'efficienza (uguale per entrambi gli scint) e scrivere su un file (se il nome è uguale a uno esistente vi chiede se sovrascrivere). Esso salva anche la fig in pdf. La distanza tra un pmt e l'altro è scritta nel programma e nel file. Potete divertirvi a generare istogrammi!

--- esp_1-flusso_di_raggi_cosmici/Marasciulli/montecarlo.py
+++ esp_1-flusso_di_raggi_cosmici/Marasciulli/montecarlo.py
@@ -3,33 +3,38 @@ try:
     os.chdir("flusso cosmici")
 except FileNotFoundError:
     pass
-    
+
 # distribuzione angolare
 
 def distro(teta):
     return  3/(2*pi)*cos(teta)**2 
 
 # cose da definire una volta sola
-tutti=10**2
-volte=10**4
-l1=40 #cm
-l2=48 #cm
-h=10 #cm
+tutti=10**4
+volte=500
+l1=40   #cm
+l2=48   
+h=20    
 eff=1
 
+try:
+    file=open("acc_2_%d.txt"%(eff*100,),"x")
+except FileExistsError:
+    a=input("cancellare? s/n \n")
+    if a=="n":
+        assert 2==0
+    
+sys.stdout=file
+    
 print("SIMULAZIONE")
 print("Due scintillatori vicini")
 print("efficienza=",eff)
+print("distanza=",h,"cm")
+print("%.0e raggi cosmici" %tutti)
+print("iterato %.0e volte" %volte)
+print("")
 
-# creo un istogramma
-
-py.figure("accettanza geometrica di 2 piani con efficienza 1 con %.0e raggi cosmici ripetuto %.0e volte" %(tutti,volte) ).set_tight_layout(True)
-py.rc("font",size=16)
-py.grid(color="black",linestyle="--")
-
-py.title("Distribuzione delle accettanze")
-py.xlabel("accettanza")
-py.ylabel("occorrenza")
+# simulazione vera
 
 acc=array([])
 
@@ -41,7 +46,6 @@ for i in range(volte):
     while (i<tutti):
         teta=random.uniform(-pi/2,pi/2)
         sul=random.uniform(0,1)  # variabile a caso che sceglie se sono dentro o meno
-        
 
         x=random.uniform(0,l1)
         y=random.uniform(0,l2) # separazione delle variabili (indipendenza da fi)
@@ -61,10 +65,22 @@ for i in range(volte):
             else:
                 c2=False
                 
-            if c1 and c2 ==True:
+            fortuna=random.uniform(0,1)
+                
+            if (c1 and c2 == True) and (fortuna<=eff):
                 buoni+=1
                 
     acc=py.append(acc,buoni/tutti)
+    
+# creo un istogramma
+    
+py.figure("acc_vicini" ).set_tight_layout(True)
+py.rc("font",size=16)
+py.minorticks_on()
+
+py.title("Distribuzione delle accettanze")
+py.xlabel("accettanza")
+py.ylabel("occorrenza")
 
 py.hist(acc,bins="auto",rwidth=0.9,normed=True)
 
@@ -72,5 +88,9 @@ acce=py.mean(acc) # suppongo che la distribuzione sia gaussiana
 err=py.std(acc,ddof=1) # e lo è se aspetti 3 ore
 
 py.show()
+py.savefig("acc_2_%d.pdf"%(eff*100,))
 
+print("accettanza geometrica=",acce*100,"+-",err*100,"%")
+sys.stdout.close()
+sys.stdout=sys.__stdout__
 print("accettanza geometrica=",acce*100,"+-",err*100,"%")
