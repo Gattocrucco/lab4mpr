c5112e0153253146245235b35ec0c20a797133b2
Giacomo Petrillo <info@giacomopetrillo.com>
2017-12-05 01:21:49 +0100

    stima intervallare poissoniana F.C. funziona per CL non troppo alti (a 0.999 sembra non siano tutti intervalli, ma fino a 0.99 ok)

--- esp-1-flusso_di_raggi_cosmici/petrillo/fcpoisson.py
+++ esp-1-flusso_di_raggi_cosmici/petrillo/fcpoisson.py
@@ -1,6 +1,7 @@
 from pylab import *
-from scipy.stats import poisson
+from scipy.stats import poisson, norm
 from scipy.special import xlogy
+from scipy.optimize import bisect
 
 # log likelihood ratio
 # (!) k >= 0, mu >= 0
@@ -10,7 +11,7 @@ poisson_order = lambda k, mu: k + xlogy(k, mu) - xlogy(k, k)
 # prefer low k
 # (!) mu >= 0, CL > 0, CL < 1
 def coverage(mu, CL):
-    mus = array([floor(mu), ceil(mu)])
+    mus = array([floor(mu), ceil(mu)], dtype='int32')
     startk = mus[argmax(poisson_order(mus, mu))]
     left = startk
     right = startk
@@ -29,22 +30,44 @@ def coverage(mu, CL):
             coverage += poisson.pmf(right, mu)
     return array([left, right])
 
-CLS = [0.68, 0.90, 0.95, 0.99]
-mus = linspace(0, 10, 1000)
+# fast but tight right bound to the band
+# (!) k >= 0, CL > 0, CL < 1
+# norm.ppf: bound for large k
+# -log(1-CL): bound for k = 0 for great CL
+# exp(-1): bound for k = 0 for small CL
+def right_bound(k, CL):
+    return k + ceil(sqrt(k) * norm.ppf((1+CL)/2)) - log(1 - CL) + exp(-1)
 
-figure('FC poisson band').set_tight_layout(True)
-clf()
+# band horizontal line
+# (!) k >= 0, CL > 0, CL < 1
+def interval(k, CL):
+    f = lambda mu: coverage(mu, CL)[1] - k + 1/2
+    mu_left = bisect(f, 0, k) if k > 0 else 0
+    f = lambda mu: coverage(mu, CL)[0] - k - 1/2
+    mu_right = bisect(f, k, right_bound(k, CL))
+    return array([mu_left, mu_right])
 
-for CL in CLS:
-    ks = empty((len(mus), 2))
-    for i in range(len(mus)):
-        ks[i] = coverage(mus[i], CL)
-    p = plot(mus, ks[:,0], '-', label='CL = %.2f' % CL)
-    plot(mus, ks[:,1], '-', color=p[0].get_color())
+if __name__ == '__main__':
+    CLS = [0.1, 0.90, 0.9999]
+    mus = linspace(0, 20, 500)
 
-title('F.C. poisson confidence band')
-xlabel('$\\mu$')
-ylabel('k')
-legend(loc='best', fontsize='small')
+    figure('FC poisson band').set_tight_layout(True)
+    clf()
 
-show()
+    for CL in CLS:
+        ks = empty((len(mus), 2))
+        for i in range(len(mus)):
+            ks[i] = coverage(mus[i], CL)
+        p = plot(mus, ks[:,0], '-', label='CL = %.4f' % CL)
+        plot(mus, ks[:,1], '-', color=p[0].get_color())
+        uk = unique(ks[:,0])
+        ints = array([interval(k, CL) for k in uk])
+        plot(ints[:,0], uk, '<', color=p[0].get_color())
+        plot(ints[:,1], uk, '>', color=p[0].get_color())
+
+    title('F.C. poisson confidence band')
+    xlabel('$\\mu$')
+    ylabel('k')
+    legend(loc='best', fontsize='small')
+
+    show()
