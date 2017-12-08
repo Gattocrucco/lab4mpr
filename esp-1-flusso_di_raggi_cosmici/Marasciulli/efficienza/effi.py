581b61b557444e434e52d0af0c51c57bff3784b7
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2017-12-08 17:16:42 +0100

    Efficienza finita e prime cose col piombo.

--- esp-1-flusso_di_raggi_cosmici/Marasciulli/efficienza/effi.py
+++ esp-1-flusso_di_raggi_cosmici/Marasciulli/efficienza/effi.py
@@ -5,33 +5,29 @@ try:
 except FileNotFoundError:
     pass
 
-#sys.stdout=open("risultati.txt","w")
+sys.stdout=open("risultati.txt","w")
 
 print("EFFICIENZE ESP. 1 \n")
 file=(3,4,5)
+dt=4e-8
 for j in range(len(file)):
-# ignoro i singoli conteggi
-    t,null1,null2,null3,c2,c3=py.loadtxt("eff_%d.txt" %file[j],unpack=True)
-    del null1,null2,null3
+    t,r1,r2,r3,c2,c3=py.loadtxt("eff_%d.txt" %file[j],unpack=True)
+    T=py.sum(t)*1000
     
-    # misura fatta con il tempo più lungo
-    due=c2[-1]
-    tre=c3[-1]
-    d_due=sqrt(due)
-    d_tre=sqrt(tre)
-    finale=uf(tre,d_tre)/uf(due,d_due)
+    cas2=py.sum( (r1*r3*dt)/T )
+    cas3=py.sum( (r1*r2*r3*dt**2)/T )  #coincidenze casuali
     
-    # media di tutte le altre (inclusa quella di cui sopra)
-    c2=uarray(c2,sqrt(c2))
-    c3=uarray(c3,sqrt(c3))
+    C2=uarray( c2,sqrt(c2) ) 
+    cont2=py.sum(C2)
     
-    effp=c3/c2
-    eff=py.mean(med(effp))
-    deff=py.mean(err(effp))
+    C3=uarray( c3,sqrt(c3) ) 
+    cont3=py.sum(C3)
     
-    print("efficienza PMT%d="%file[j],eff,"+-",deff)
-    print("misura migliore={:.1u}".format(finale))
-    print("")
+    eff=cont3/cont2
     
-#sys.stdout.close()
-#sys.stdout=sys.__stdout__
+    print("efficienza PMT%d="%file[j],"{:.1u}".format(eff))
+    print("coincidenze casuali a 2= %.0e" %cas2)
+    print("coincidenze casuali a 3= %.0e" %cas3,"\n")
+    
+sys.stdout.close()
+sys.stdout=sys.__stdout__
--- esp-1-flusso_di_raggi_cosmici/Marasciulli/piombo/materiale.py
+++ esp-1-flusso_di_raggi_cosmici/Marasciulli/piombo/materiale.py
@@ -9,23 +9,24 @@ n,t,tre,quattro,cinque,sei,c3=py.loadtxt("lastre_1.txt",unpack=True)
 del tre,quattro,cinque,sei
 
 num=[0,1,2,4]
-l=array([])
-dl=array([])
+l=uarray([],[])
 for N in num:
-    cont=c3[n==N]/t[n==N]*1000
-    l=py.append(l, py.average(cont,weights=1/cont) )
-    dl=py.append(dl, astd(sqrt(cont)) )
-    
+    C3=uarray( c3[n==N],sqrt(c3[n==N]) ) 
+    cont=py.sum(C3)/py.sum(t[n==N])*1000
+    l=py.append(l,cont)
+
 py.figure(1).set_tight_layout(True)
 py.rc("font",size=16)
 py.grid(color="black",linestyle="--")
-py.minorticks_off()
+py.minorticks_on()
 
 py.title("Misura con fogli di piombo",size=18)
 py.xlabel("numero di fogli")
 py.ylabel("conteggi normalizzati")  # intendo nello stesso intervallo di tempo
 
-py.plot([0,4],[py.average(l,weights=1/dl)]*2,linestyle="--",color="black")
-py.errorbar(num,l,yerr=dl,color="black",linestyle="",marker="o",capsize=2)
+z=py.linspace(-0.2,4.2,500)
+py.errorbar(z,[py.average( (med(l)),weights=1/err(l)**2 )]*len(z),linestyle="",color="gray",yerr=astd(err(l)),ecolor="yellow")
+py.plot(py.xlim(),[py.average( (med(l)),weights=1/err(l)**2 )]*2,linestyle="--",color="black")
+py.errorbar(num,med(l),yerr=err(l),color="black",linestyle="",marker="o",capsize=2)
 
 py.show()
