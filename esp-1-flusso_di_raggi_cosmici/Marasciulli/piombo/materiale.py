433aedfaefe330b3f6ff4161bef1f5fb4120f861
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2017-12-08 18:57:57 +0100

    Conteggi nel piombo
    Confronto tra coincidenze a 2 e a 3.
    NOTA IMPORTANTE: le coincidenze a 2 non possono essere confrontate direttamente con quelle a 3 a causa dell'accettanza geometrica. Quella calcolata dal montecarlo non va bene perché le coincidenze a 3 scattano soltanto quando anche quelle a 2 sono scattate, quindi dal PMT superiore arrivano solo quelle che si trovano in un cono più ristretto. Per risolvere questo problema ho fatto coinc3/coinc2 quando non c'erano le lastre e l'ho usato come fattore di conversione.
    Ho aggiunto questa spiegazione al logbook nella sezione in cui c'è la tabella dei dati.

--- esp-1-flusso_di_raggi_cosmici/Marasciulli/piombo/materiale.py
+++ esp-1-flusso_di_raggi_cosmici/Marasciulli/piombo/materiale.py
@@ -5,15 +5,25 @@ except FileNotFoundError:
     pass
 
 
-n,t,tre,quattro,cinque,sei,c3=py.loadtxt("lastre_1.txt",unpack=True)
-del tre,quattro,cinque,sei
+n,t,r5,r4,r3,c2,c3=py.loadtxt("lastre_1.txt",unpack=True)
 
-num=[0,1,2,4]
-l=uarray([],[])
+num=array([0,1,2,4])
+l3=uarray([],[])
+l2=uarray([],[])
+dt=4e-8
 for N in num:
+    T=py.sum(t[n==N])/1000
+    
     C3=uarray( c3[n==N],sqrt(c3[n==N]) ) 
-    cont=py.sum(C3)/py.sum(t[n==N])*1000
-    l=py.append(l,cont)
+    cont3=py.sum(C3)/T
+    l3=py.append(l3,cont3)
+    
+    C2=uarray( c2[n==N],sqrt(c2[n==N]) ) 
+    cont2=py.sum(C2)/T
+    l2=py.append(l2,cont2)
+    
+    if N==0:
+        corre=cont3/cont2
 
 py.figure(1).set_tight_layout(True)
 py.rc("font",size=16)
@@ -24,9 +34,20 @@ py.title("Misura con fogli di piombo",size=18)
 py.xlabel("numero di fogli")
 py.ylabel("conteggi normalizzati")  # intendo nello stesso intervallo di tempo
 
-z=py.linspace(-0.2,4.2,500)
-py.errorbar(z,[py.average( (med(l)),weights=1/err(l)**2 )]*len(z),linestyle="",color="gray",yerr=astd(err(l)),ecolor="yellow")
-py.plot(py.xlim(),[py.average( (med(l)),weights=1/err(l)**2 )]*2,linestyle="--",color="black")
-py.errorbar(num,med(l),yerr=err(l),color="black",linestyle="",marker="o",capsize=2)
+l2=l2*corre
 
+# punti coincidenze a 2
+py.errorbar(num+0.1,med(l2),yerr=err(l2),color="red",linestyle="",marker=".",capsize=2,label="coincidenze a 2")
+# punti coincidenze a 3
+py.errorbar(num,med(l3),yerr=err(l3),color="black",linestyle="",marker=".",capsize=2,label="coincidenze a 3")
+
+'''
+lim=py.xlim()
+z=py.linspace(lim[0],lim[1],500)
+# linea gialla
+py.errorbar(z,[py.average( (med(l3)),weights=1/err(l3)**2 )]*len(z),linestyle="",color="gray",yerr=astd(err(l3)),ecolor="yellow")
+#linea tratteggiata
+py.plot(py.xlim(),[py.average( (med(l3)),weights=1/err(l3)**2 )]*2,linestyle="--",color="black")
+'''
+py.legend(loc="best",fontsize="small")
 py.show()
