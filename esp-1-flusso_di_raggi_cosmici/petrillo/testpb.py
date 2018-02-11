09e4d076d6c61f93d64073843d2918d7be5982a8
Giacomo Petrillo <info@giacomopetrillo.com>
2018-02-11 01:58:03 +0100

    script energia piombo

--- esp-1-flusso_di_raggi_cosmici/petrillo/testpb.py
+++ esp-1-flusso_di_raggi_cosmici/petrillo/testpb.py
@@ -6,6 +6,8 @@ print('loading data...')
 data1 = loadtxt('../de0_data/piombo_con_tutto_c12.dat', unpack=True, usecols=(1,))
 data2 = loadtxt('../de0_data/piombo_senza_c12.dat', unpack=True, usecols=(1,))
 
+############ MEAN AND MODE ############
+
 def mode(data):
     counts, bins = histogram(data, bins='sqrt')
     m_idx = argmax(counts)
@@ -25,13 +27,68 @@ mu2 = average(data2)
 print('piombo: moda = {}\nsenza piombo: moda = {}'.format(m1, m2))
 print('piombo: media = {}\nsenza piombo: media = {}'.format(mu1, mu2))
 
+############ PLOT ############
+
+nbins = int(sqrt(min(len(data2), len(data1))))
+
+# crea istogrammi
+MIN = min(min(data1), min(data2))
+MAX = max(max(data1), max(data2))
+counts1, edges = histogram(data1, bins=nbins, range=(MIN, MAX))
+counts2, edges = histogram(data2, bins=nbins, range=(MIN, MAX))
+counts1 = counts1 / len(data1) / (MAX - MIN)
+counts2 = counts2 / len(data2) / (MAX - MIN)
+
+figure('piombo energia').set_tight_layout(False)
+clf()
+
+# plot istogrammi insieme
+subplot(211)
+bar(edges[:-1], counts2, log=False, align='edge', width=(MAX-MIN)/nbins, color='lightgray', label='senza piombo')
+dup_edges = empty(2 * len(edges))
+dup_edges[2 * arange(len(edges))] = edges
+dup_edges[2 * arange(len(edges)) + 1] = edges
+dup_counts = zeros(2 * len(edges))
+dup_counts[2 * arange(len(edges) - 1) + 1] = counts1
+dup_counts[2 * arange(len(edges) - 1) + 2] = counts1
+plot(dup_edges, dup_counts, '-k', label='con piombo', linewidth=1)
+ylabel('densità [arb.un.$^{-1}$]')
+legend(loc=0, fontsize='small')
+xticks([])
+
+# plot differenza istogrammi
+subplot(212)
+# @numba.jit(numba.float64[:](numba.float64[:], numba.float64[:], numba.float64[:]), cache=True, nopython=True)
+# def diff_cumulant(x, data1, data2):
+#     rt = empty(len(x))
+#     for i in range(len(x)):
+#         rt[i] = sum(data1 < x[i]) / len(data1) - sum(data2 < x[i]) / len(data2)
+#     return rt
+dup_counts[:] = 0
+dup_counts[2 * arange(len(edges) - 1) + 1] = counts1 - counts2
+dup_counts[2 * arange(len(edges) - 1) + 2] = counts1 - counts2
+plot(dup_edges, dup_counts, '-k', label='piombo $-$ senza piombo', linewidth=1)
+centers = edges[:-1] + (edges[1]-edges[0])/2
+def error(counts, data):
+    return sqrt(counts) / len(data) / (MAX - MIN)
+errors = sqrt(error(counts1, data1)**2 + error(counts2, data2)**2)
+errorbar(centers, counts1 - counts2, yerr=errors, fmt=',k', capsize=0)
+legend(loc=0, fontsize='small')
+ylabel('densità [arb.un.$^{-1}$]')
+
+xlabel('energia [arb.un.]')
+
+show()
+
+############ KOLMOGOROV-SMIRNOV TEST ############
+
 data1 = sort(data1)
 data2 = sort(data2)
 
 @numba.jit(cache=True, nopython=True)
 def oneside(dataa, datab):
     # dataa and datab must be sorted
-    M = 0.
+    M = 0
     j = 0
     for i in range(len(dataa)):
         while j < len(datab) and datab[j] <= dataa[i]:
@@ -53,5 +110,3 @@ alpha = 1e-7
 l = level(alpha, len(data1), len(data2))
 
 print('D = %f, l = %f (alpha = %g)' % (D, l, alpha))
-
-
