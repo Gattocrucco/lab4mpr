5e9aa328d5702b56bde7ed3156e0e3401d247438
Giacomo Petrillo <info@giacomopetrillo.com>
2018-02-23 17:20:29 +0100

    logtonpy.py converte da .log a .npy

---
+++ esp-2-Compton/petrillo/logtonpy.py
@@ -0,0 +1,31 @@
+import numpy as np
+import sys
+import scanf
+
+def clear_lines(nlines, nrows):
+    for i in range(nlines):
+        sys.stdout.write('\033[F\r%s\r' % (" " * nrows,))
+    sys.stdout.flush()
+
+filename = sys.argv[1]
+printevery = 100000
+
+values = np.empty(2, dtype='uint16')
+
+with open(filename, 'r') as logfile:
+    i = 0
+    for line in logfile:
+        value = int(line, base=16)
+        if i >= len(values):
+            values.resize(len(values) * 2)
+        values[i] = value
+        i += 1
+        if i % printevery == 0:
+            if i > printevery:
+                clear_lines(1, 70)
+            print('lines: %d' % i)
+
+values.resize(i)
+npyfilename = filename.replace('.log', '.npy')
+print('saving as uint16 array in file %s' % npyfilename)
+np.save(npyfilename, values)
