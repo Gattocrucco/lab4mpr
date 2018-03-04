012992a8473b3d66c84e28094a12e0fd9bce0976
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-04 14:21:09 +0100

    progressi sulla funzione per fittare la spalla

--- esp-2-Compton/petrillo/empirical.py
+++ esp-2-Compton/petrillo/empirical.py
@@ -19,14 +19,17 @@ else:
 def gauss(x, mu, sigma):
     return np.exp(-1/2 * (x - mu)**2 / sigma**2)
 
+def log_gauss(x, s, scale):
+    return np.where(x > 0, 1 / (x / scale) * np.exp(-1/2 * (np.log(x / scale) / s)**2), 0)
+
 def fermi(x, x0, scale):
     return 1 / (1 + np.exp((x - x0) / scale))
 
-def parabola(x, x0, slope, curv):
-    return 1 + slope * (x - x0) + curv * (x - x0)**2
+def slope(x, x0, slope):
+    return 1 + slope * (x - x0)
 
 def empirical_secondary(e, *p):
-    return p[0] * gauss(e, p[1], p[2]) + p[3] * fermi(e, p[4], p[5])
+    return p[0] * gauss(e, p[1], p[2]) + p[3] * fermi(e, p[4], p[5]) * slope(e, p[4], p[6]) + p[7] * log_gauss(-(e - p[8]) + p[9], p[10] / p[9], p[9])
 
 fig = plt.figure('empirical')
 fig.clf()
@@ -36,9 +39,10 @@ counts, edges = np.histogram(secondary, bins='sqrt')
 norm_counts = counts / (np.mean(counts) * (edges[-1] - edges[0]))
 histo.bar_line(edges, norm_counts, ax=ax)
 
-p = (2.55, 0.53, 0.035, 1.4, 0.5, 0.02)
+p = (2.55, 0.53, 0.035, 1.25, 0.44, 0.02, -1, 1.1, 0.45, 0.15, 0.04)
 ax.plot(edges, p[0] * gauss(edges, p[1], p[2]), '--k')
-ax.plot(edges, p[3] * fermi(edges, p[4], p[5]), '--k')
+ax.plot(edges, p[3] * fermi(edges, p[4], p[5]) * slope(edges, p[4], p[6]), '--k')
+ax.plot(edges, p[7] * log_gauss(-(edges - p[8]) + p[9], p[10] / p[9], p[9]), '--k')
 ax.plot(edges, empirical_secondary(edges, *p), '-k')
 
 ax_di = fig.add_subplot(212)
