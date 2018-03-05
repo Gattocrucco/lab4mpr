41bd062fecdbcd24c545a1c442c4fa0f6d33959c
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-05 15:49:04 +0100

    interfaccia esterna empirical.py

--- esp-2-Compton/petrillo/empirical.py
+++ esp-2-Compton/petrillo/empirical.py
@@ -1,25 +1,8 @@
 import numpy as np
-import mc9
-import os
 from scipy import stats
 import matplotlib.pyplot as plt
 import histo
 import lab
-
-if not os.path.exists('empirical.npz'):
-    print('running mc9...')
-    kw = dict(N=1000000)
-    A = []
-    A.append(mc9.mc_cal(1.33, theta_0=45, m_e=.3, seed=1, **kw))
-    print('saving in empirical.npz...')
-    primary = [a[0] for a in A]
-    secondary = [a[1] for a in A]
-    np.savez('empirical.npz', primary=primary, secondary=secondary)
-
-print('loading empirical.npz...')
-data = np.load('empirical.npz')
-primary = data['primary']
-secondary = data['secondary']
 
 def gauss(x, mu, sigma):
     return np.exp(-1/2 * (x - mu)**2 / sigma**2)
@@ -33,55 +16,92 @@ def fermi(x, x0, scale):
 def slope(x, x0, slope):
     return 1 + slope * (x - x0)
 
-f1 = lambda e, *p: p[0] * gauss(e, p[1], p[1] * p[2])
-f2 = lambda e, *p: p[0] * p[3] * fermi(e, p[1] * p[4], p[1] * p[5]) * slope(e, p[1] * p[4], 1/p[1] * p[6])
-f3 = lambda e, *p: p[0] * p[7] * log_gauss(-(e - p[1] * p[8]) + p[1] * p[9], p[10] / p[9], p[1] * p[9])
+f1 = lambda e, *p: p[1] * gauss(e, p[0], p[0] * p[2])
+f2 = lambda e, *p: p[1] * p[3] * fermi(e, p[0] * p[4], p[0] * p[5]) * slope(e, p[0] * p[4], 1/p[0] * p[6])
+f3 = lambda e, *p: p[1] * p[7] * log_gauss(-(e - p[0] * p[8]) + p[0] * p[9], p[10] / p[9], p[0] * p[9])
 
 def empirical_secondary(e, *p):
     return f1(e, *p) + f2(e, *p) + f3(e, *p)
 
-fig = plt.figure('empirical')
-fig.clf()
+class EmpiricalSecondary(object):
 
-ax = fig.add_subplot(211)
-ax_di = fig.add_subplot(212)
+    def __init__(self, samples, plot=False):
+        """
+        samples is <secondary> output from mc9.mc
+        """
+        if plot:
+            fig = plt.figure('empirical')
+            fig.clf()
 
-for i in range(len(secondary)):
-    counts, edges = np.histogram(secondary[i], bins='sqrt')
-    norm_factor = 1 / (np.mean(counts) * (edges[-1] - edges[0]))
-    norm_counts = counts * norm_factor
-    histo.bar_line(edges, norm_counts, ax=ax, label='{}'.format(i))
-    x = ax.get_xlim()
-    y = ax.get_ylim()
-    # if i != 0: continue
+            ax = fig.add_subplot(211)
+            ax_di = fig.add_subplot(212)
+
+        counts, edges = np.histogram(samples, bins='sqrt')
+        norm_factor = 1 / (np.mean(counts) * (edges[-1] - edges[0]))
+        norm_counts = counts * norm_factor
+        if plot:
+            histo.bar_line(edges, norm_counts, ax=ax)
+            x = ax.get_xlim()
+            y = ax.get_ylim()
+
+        # estimate initial parameters
+        p = [np.nan, np.nan, np.nan, np.nan, 0.83, 0.04, np.nan, 0.43, 0.85, 0.3, 0.07]
+        idx = np.argmax(counts[len(counts) // 2:]) + len(counts) // 2
+        p[1] = norm_counts[idx] # maximum of the gaussian
+        p[0] = edges[idx] # mean of the gaussian
+        idx_hwhm = np.sum(norm_counts[idx:] >= p[1] / 2) + idx
+        p[2] = (edges[idx_hwhm] - p[0]) / (1.17 * p[0]) # sd / mean of the gaussian
+        par, _ = lab.fit_linear(edges[:len(counts) // 3], norm_counts[:len(counts) // 3])
+        p[3] = (par[0] * p[0] * p[4] + par[1]) / p[1] # amplitude of f2
+        p[6] = par[0] * p[0] / (p[1] * p[3]) # slope at left
+
+        if plot:
+            ax.plot(edges, f1(edges, *p), '--k', linewidth=0.5)
+            ax.plot(edges, f2(edges, *p), '--k', linewidth=0.5)
+            ax.plot(edges, f3(edges, *p), '--k', linewidth=0.5)
+            ax.plot(edges, empirical_secondary(edges, *p), '-k')
+            ax.set_xlim(x)
+            ax.set_ylim(y)
+            
+            ax_di.plot(edges[:-1], norm_counts - empirical_secondary(edges[:-1], *p), '-k')
+
+        out = lab.fit_curve(empirical_secondary, edges[:-1] + (edges[1] - edges[0]) / 2, norm_counts, dy=np.where(counts > 0, np.sqrt(counts), 1) * norm_factor, p0=p, print_info=plot)
+        
+        if plot:
+            ax.plot(edges, f1(edges, *out.par), '--r', linewidth=0.5)
+            ax.plot(edges, f2(edges, *out.par), '--r', linewidth=0.5)
+            ax.plot(edges, f3(edges, *out.par), '--r', linewidth=0.5)
+            ax.plot(edges, empirical_secondary(edges, *out.par), '-r')
+
+            ax_di.plot(edges[:-1], norm_counts - empirical_secondary(edges[:-1], *out.par), '-r')
+
+            fig.show()
+        
+        self._parameters = out.par
     
-    # estimate initial parameters
-    p = [np.nan, np.nan, np.nan, 0.49, 0.83, 0.04, np.nan, 0.43, 0.85, 0.3, 0.07]
-    idx = np.argmax(counts[len(counts) // 2:]) + len(counts) // 2
-    p[0] = norm_counts[idx] # maximum of the gaussian
-    p[1] = edges[idx] # mean of the gaussian
-    idx_hwhm = np.sum(norm_counts[idx:] >= p[0] / 2) + idx
-    p[2] = (edges[idx_hwhm] - p[1]) / (1.17 * p[1]) # sd / mean of the gaussian
-    par, _ = lab.fit_linear(edges[:len(counts) // 3], norm_counts[:len(counts) // 3])
-    p[3] = (par[0] * p[1] * p[4] + par[1]) / p[0] # amplitude of f2
-    p[6] = par[0] * p[1] / (p[0] * p[3]) # slope at left
+    def __call__(self, x, scale):
+        return 1/scale * empirical_secondary(x, scale * self._parameters[0], *self._parameters[1:])
+
+if __name__ == '__main__':
+    import mc9
+    import os
     
-    ax.plot(edges, f1(edges, *p), '--k', linewidth=0.5)
-    ax.plot(edges, f2(edges, *p), '--k', linewidth=0.5)
-    ax.plot(edges, f3(edges, *p), '--k', linewidth=0.5)
-    ax.plot(edges, empirical_secondary(edges, *p), '-k')
-    ax.set_xlim(x)
-    ax.set_ylim(y)
-
-    ax_di.plot(edges[:-1], norm_counts - empirical_secondary(edges[:-1], *p), '-k')
-
-    out = lab.fit_curve(empirical_secondary, edges[:-1] + (edges[1] - edges[0]) / 2, norm_counts, dy=np.where(counts > 0, np.sqrt(counts), 1) * norm_factor, p0=p, print_info=1)
-
-    ax.plot(edges, f1(edges, *out.par), '--r', linewidth=0.5)
-    ax.plot(edges, f2(edges, *out.par), '--r', linewidth=0.5)
-    ax.plot(edges, f3(edges, *out.par), '--r', linewidth=0.5)
-    ax.plot(edges, empirical_secondary(edges, *out.par), '-r')
-
-    ax_di.plot(edges[:-1], norm_counts - empirical_secondary(edges[:-1], *out.par), '-r')
-
-fig.show()
+    filename = 'empirical-test.npy'
+    if not os.path.exists(filename):
+        print('running mc9...')
+        _, samples = mc9.mc_cal(1.33, theta_0=0, N=1000000)
+        np.save(filename, samples)
+    samples = np.load(filename)
+    
+    empirical = EmpiricalSecondary(samples, plot=True)
+    
+    fig = plt.figure('empirical-test')
+    fig.clf()
+    ax = fig.add_subplot(111)
+    fx = np.linspace(np.min(samples), np.max(samples), 500)
+    ax.plot(fx, empirical(fx, 1), label='scale = 1')
+    ax.plot(fx * 2, empirical(fx * 2, 2), label='scale = 2')
+    ax.plot(fx / 2, empirical(fx / 2, 1/2), label='scale = 1/2')
+    ax.legend()
+    fig.show()
+    
