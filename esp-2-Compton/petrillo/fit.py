80f19668518f93901df583c4de8baae653849f5b
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-05 18:01:46 +0100

    iniziato script fit Mi sa che mi servirà calcolare anche i rapporti dei vari processi per riuscire a fare il fit.

---
+++ esp-2-Compton/petrillo/fit.py
@@ -0,0 +1,20 @@
+import numpy as np
+import mc9
+import empirical
+import matplotlib.pyplot as plt
+import histo
+
+pa, sa = mc9.mc_cached(1.33, theta_0=40, N=100000, seed=0)
+pb, sb = mc9.mc_cached(1.17, theta_0=40, N=100000, seed=1)
+
+samples = np.concatenate([pa, sa, pb, sb])
+
+counts = np.bincount(np.asarray(samples, dtype='u2'), minlength=2**13)[:2**13]
+
+fig = plt.figure('fit')
+fig.clf()
+ax = fig.add_subplot(111)
+
+histo.bar_line(np.arange(2**13 + 1), counts, ax=ax)
+
+fig.show()
