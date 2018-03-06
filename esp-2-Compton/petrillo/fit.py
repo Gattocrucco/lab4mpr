4f9354bf0ea26f420a9117b998bd4a82185a246e
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-06 22:21:58 +0100

    aggiunti bounds al fit A volte mi venivano delle normalizzazioni negative.

--- esp-2-Compton/petrillo/fit.py
+++ esp-2-Compton/petrillo/fit.py
@@ -7,8 +7,8 @@ import lab
 import sympy as sp
 
 print('monte carlo...')
-pa, sa = mc9.mc_cached(1.33, theta_0=0, N=1000000, seed=0)
-pb, sb = mc9.mc_cached(1.17, theta_0=0, N=1000000, seed=1)
+pa, sa = mc9.mc_cached(1.33, theta_0=50, N=1000000, seed=0)
+pb, sb = mc9.mc_cached(1.17, theta_0=50, N=1000000, seed=1)
 
 edges = np.arange(2**13 + 1)
 
@@ -33,10 +33,15 @@ def fit_fun_b(e, N1, mu1, sigma1, Ns1, scale1, N2, mu2, sigma2, Ns2, scale2):
 def fit_fun(e, *p):
     return fit_fun_a(e, *p) + fit_fun_b(e, *p)
 
+print('fit...')
 p0 = [len(pa), np.mean(pa), np.std(pa), len(sa), 1, len(pb), np.mean(pb), np.std(pb), len(sb), 1]
+bounds = [
+	 [0,       -np.inf,     0,          0,       0, 0,       -np.inf,     0,          0,       0],
+	 [np.inf] * len(p0)
+]
 
 for i in range(len(p0)):
-    p0[i] = np.random.uniform(0.95 * p0[i], 1.05 * p0[i])
+    p0[i] = np.random.uniform(0.9 * p0[i], 1.1 * p0[i])
 
 cut = edges[:-1] >= 100
 fit_x = edges[:-1][cut] + 0.5
@@ -44,7 +49,7 @@ fit_y = counts[cut]
 fit_dy = np.where(fit_y > 0, np.sqrt(fit_y), 1)
 
 model = lab.CurveModel(fit_fun, symb=True, npar=len(p0))
-out = lab.fit_curve(model, fit_x, fit_y, dy=fit_dy, p0=p0, print_info=1)
+out = lab.fit_curve(model, fit_x, fit_y, dy=fit_dy, p0=p0, print_info=1, bounds=bounds)
 
 fig = plt.figure('fit')
 fig.clf()
