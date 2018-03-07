0b102a8c1c3d548a7312458027c46d67c6d61b72
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-07 18:13:07 +0100

    modifiche al montecarlo * La cache di mc_cached sta tutta in una cartella (mc9_cached); * Il montecarlo usa la geometria 3D del NaI e oltre ai campioni di energie restituisce le corrispondenti probabilità. Attenzione a come normalizzate quando fate l'istogramma.

--- esp-2-Compton/petrillo/mc9.py
+++ esp-2-Compton/petrillo/mc9.py
@@ -5,6 +5,7 @@ import calibration
 import os
 import pickle
 import lab
+import cross as cs # conflict with cross()
 
 def make_von_neumann(density, domain, max_cycles=100000):
     """
@@ -83,7 +84,7 @@ def energy_nai(E, *a, **k):
     return E_cal + random_normal(len(E), seed=seed) * sigma
 
 @nb.jit(nopython=True, cache=True)
-def mc(energy, theta_0=0, N=1000, seed=-1, beam_sigma=1.74, beam_center=0, nai_distance=40, nai_radius=2.54, m_e=0.511, acc_bounds=True, max_secondary_cos_theta=1):
+def mc(energy, theta_0=0, N=1000, seed=-1, beam_sigma=1.74, beam_center=-0.09, nai_distance=40, nai_radius=2.54, nai_depth=5.08, nai_density=3.667, m_e=0.511, acc_bounds=True, geometry_3d=True, max_secondary_cos_theta=1):
     """
     Simulate Compton scattering on the target and energy measurement of scattered photon with NaI.
     
@@ -102,21 +103,33 @@ def mc(energy, theta_0=0, N=1000, seed=-1, beam_sigma=1.74, beam_center=0, nai_d
     beam_center : float
         Center of the beam as polar angle from experiment axis, in degrees.
     nai_distance : float
-        Distance of the NaI from the target.
+        Distance of the NaI from the target, in cm.
     nai_radius : float
-        Radius of the NaI, in the same units as nai_distance.
+        Radius of the NaI, in cm.
+    nai_depth : float
+        Depth of the NaI, i.e. distance from side facing the target to the other side, in cm.
+    nai_density : float
+        Density of NaI, in g/cm^3.
     m_e : float
         Electron mass, in MeV.
+    geometry_3d : bool
+        If False, use a simplified 2d model of the NaI without keeping into account
+        interaction probabilities of the photon.
     acc_bounds : bool
         If False, disable quick bounds on NaI shape that speed up montecarlo.
     
     Returns
     -------
-    primary_photons :
+    primary_photons : (N) float array
         Energies of Compton photons from target hitting the NaI.
-    secondary_electrons :
+    secondary_electrons : (N) float array
         Kinetical energies of electrons emitted from Compton in the NaI.
+    weights_pp : (N) float array
+    weights_se : (N) float array
+        Weights of primary photons and secondary electron samples, respectively.
+        See for example numpy.histogram for the meaning.
     """
+    # convert from degrees to radians
     beam_sigma *= np.pi / 180
     beam_center *= np.pi / 180
     theta_0 *= np.pi / 180
@@ -131,10 +144,18 @@ def mc(energy, theta_0=0, N=1000, seed=-1, beam_sigma=1.74, beam_center=0, nai_d
     y = Y
     x = X * np.cos(theta_0) - Z * np.sin(theta_0)
     
+    # seed
     if seed >= 0:
         np.random.seed(seed)
+    
     primary_photons = np.empty(N)
     secondary_electrons = np.empty(N)
+    if geometry_3d:
+        weights_pp = np.empty(N)
+        weights_se = np.empty(N)
+    else:
+        weights_pp = np.ones(N)
+        weights_se = np.ones(N)
     i = 0
     count = 0
     while i < N:
@@ -199,10 +220,23 @@ def mc(energy, theta_0=0, N=1000, seed=-1, beam_sigma=1.74, beam_center=0, nai_d
         
         rho = z_f * cos_theta + sin_theta * (x_f * np.cos(phi) + y_f * np.sin(phi))
         rho_z = np.dot(rho, z)
-        radius2 = nai_distance**2 * (1 - rho_z**2) / rho_z**2
+        radius = nai_distance * np.sqrt(1 - rho_z**2) / rho_z
         
-        if radius2 <= nai_radius**2 and rho_z >= 0:
+        # condition: the photon intersects the front face of the NaI
+        if 0 <= radius <= nai_radius:
             primary_photon = compton_photon_energy(energy, cos_theta, m_e)
+            
+            if geometry_3d:
+                # compute the length of the trajectory of the photon inside the NaI
+                # (assuming it wouldn't interact)
+                radius_back = (nai_distance + nai_depth) / nai_distance * radius
+                l = np.sqrt(nai_depth**2 + (radius_back - radius)**2)
+                if radius_back > nai_radius:
+                    l *= (nai_radius - radius) / (radius_back - radius)
+            
+                # compute probabilities of interaction
+                weights_pp[i] = 1 - np.exp(-l * cs.photoel(primary_photon) * nai_density)
+                weights_se[i] = 1 - np.exp(-l * cs.compton(primary_photon) * nai_density)
             
             # extract theta of secondary compton photon
             while 1:
@@ -222,16 +256,16 @@ def mc(energy, theta_0=0, N=1000, seed=-1, beam_sigma=1.74, beam_center=0, nai_d
             i += 1
         count += 1
         
-    return primary_photons, secondary_electrons
+    return primary_photons, secondary_electrons, weights_pp, weights_se
 
 def mc_cal(*args, **kwargs):
     """
     Same as mc, but applies calibration.
     """
-    p, s = mc(*args, **kwargs)
+    p, s, wp, ws = mc(*args, **kwargs)
     p = energy_nai(p)
     s = energy_nai(s)
-    return p, s
+    return p, s, wp, ws
 
 def mc_cached(*args, **kwargs):
     """
@@ -257,7 +291,10 @@ def mc_cached(*args, **kwargs):
         raise ValueError('Argument seed={} must be >= 0.'.format(kwargs['seed']))
     
     # load or create database
-    database_file = 'mc9_cache.pickle'
+    directory = 'mc9_cache'
+    database_file = '{}/mc9_cache.pickle'.format(directory)
+    if not os.path.exists(directory):
+        os.mkdir(directory)
     if os.path.exists(database_file):
         with open(database_file, 'rb') as file:
             database = pickle.load(file)
@@ -273,10 +310,12 @@ def mc_cached(*args, **kwargs):
         data = np.load(data_file)
         p = data['primary']
         s = data['secondary']
+        wp = data['weights_p']
+        ws = data['weights_s']
     else:
-        p, s = mc(*args, **kwargs)
-        new_file = lab.nextfilename('mc9_cache', '.npz')
-        np.savez(new_file, primary=p, secondary=s)
+        p, s, wp, ws = mc(*args, **kwargs)
+        new_file = lab.nextfilename('mc9_cache', '.npz', prepath=directory)
+        np.savez(new_file, primary=p, secondary=s, weights_p=wp, weights_s=ws)
         database[hashable_args] = new_file
     
     # save database
@@ -288,16 +327,16 @@ def mc_cached(*args, **kwargs):
         p = energy_nai(p, seed=4000000000 + kwargs['seed'])
         s = energy_nai(s, seed=2000000000 + kwargs['seed'])
     
-    return p, s
+    return p, s, wp, ws
 
 if __name__ == '__main__':
     N=100000
-    primary, secondary = mc_cached(1.33, theta_0=0, N=N, seed=0)
+    p, s, wp, ws = mc_cached(1.33, theta_0=90, N=N, seed=0)
     
     from matplotlib.pyplot import *
     figure('mc9')
     clf()
-    hist(primary, bins='sqrt', histtype='step', label='primary', density=True)
-    hist(secondary, bins='sqrt', histtype='step', label='secondary', density=True)
+    hist(p, bins=int(np.sqrt(N)), weights=wp / (N * (np.max(wp) - np.min(wp))), histtype='step', label='photoel')
+    hist(s, bins=int(np.sqrt(N)), weights=ws / (N * (np.max(ws) - np.min(ws))), histtype='step', label='compton')
     legend(loc=1)
     show()
