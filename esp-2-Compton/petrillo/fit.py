499089f60be1609ebe5c9492401d11644f9f4963
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-10 00:33:07 +0100

    aggiunto fit con il fondo esponenziale e basta

--- esp-2-Compton/petrillo/fit.py
+++ esp-2-Compton/petrillo/fit.py
@@ -44,6 +44,8 @@ fig.clf()
 
 centers_133 = []
 centers_117 = []
+centers_133_sim = []
+centers_117_sim = []
 
 for i in range(len(files)):
 	filename = files[i]
@@ -118,6 +120,18 @@ for i in range(len(files)):
 	except:
 		par = p0
 		print('fit failed!')
+	
+	print('simplified background fit...')
+	p0_sim = np.copy(p0)
+	pfix = [3, 8]
+	p0_sim[pfix] = 0
+	pfix += [4, 9]
+	# try:
+	out_sim = lab.fit_curve(model, fit_x, fit_y, dy=fit_dy, p0=p0_sim, pfix=pfix, print_info=3, method='linodr', bounds=bounds)
+	par_sim = out_sim.par
+	# except:
+	# 	par_sim = p0_sim
+	# 	print('fit failed!')
 
 	print('plot...')
 	modela = lab.CurveModel(fit_fun_a, symb=True)
@@ -128,10 +142,17 @@ for i in range(len(files)):
 	rebin_counts = histo.partial_sum(counts, rebin)
 	histo.bar_line(edges, rebin_counts, ax=ax, label=filename.split('/')[-1])
 	ax.plot(fit_x,  model.f()(fit_x, *p0), '-k')
+	ax.plot(fit_x, modela.f()(fit_x, *p0), '--k', linewidth=0.5)
+	ax.plot(fit_x, modelb.f()(fit_x, *p0), '--k', linewidth=0.5)
+	ax.plot(fit_x, modelc.f()(fit_x, *p0), '--k', linewidth=0.5)
 	ax.plot(fit_x,  model.f()(fit_x, *par), '-r')
 	ax.plot(fit_x, modela.f()(fit_x, *par), '--r', linewidth=0.5)
 	ax.plot(fit_x, modelb.f()(fit_x, *par), '--r', linewidth=0.5)
 	ax.plot(fit_x, modelc.f()(fit_x, *par), '--r', linewidth=0.5)
+	ax.plot(fit_x,  model.f()(fit_x, *par_sim), '-g')
+	ax.plot(fit_x, modela.f()(fit_x, *par_sim), '--g', linewidth=0.5)
+	ax.plot(fit_x, modelb.f()(fit_x, *par_sim), '--g', linewidth=0.5)
+	ax.plot(fit_x, modelc.f()(fit_x, *par_sim), '--g', linewidth=0.5)
 	ax.grid()
 	ax.legend(fontsize='small', loc=2)
 	ymax = np.max(rebin_counts[15:1000])
@@ -141,6 +162,9 @@ for i in range(len(files)):
 	c133, c117 = un.correlated_values(out.par[idx], out.cov[np.ix_(idx,idx)], tags=['fit'] * 2)
 	centers_133.append(c133)
 	centers_117.append(c117)
+	c133_sim, c117_sim = un.correlated_values(out_sim.par[idx], out_sim.cov[np.ix_(idx,idx)], tags=['fit'] * 2)
+	centers_133_sim.append(c133_sim)
+	centers_117_sim.append(c117_sim)
 	
 def fun_energy(E_0, m_e, theta_0):
 	return E_0 / (1 + E_0 / m_e * (1 - np.cos(np.radians(theta_0))))
@@ -148,22 +172,30 @@ def fun_energy(E_0, m_e, theta_0):
 for i in range(len(calib_date)):
 	centers_133[i] = calibration.energy_inverse_calibration(calib_date[i], unc=True)(centers_133[i])
 	centers_117[i] = calibration.energy_inverse_calibration(calib_date[i], unc=True)(centers_117[i])
+	centers_133_sim[i] = calibration.energy_inverse_calibration(calib_date[i], unc=True)(centers_133_sim[i])
+	centers_117_sim[i] = calibration.energy_inverse_calibration(calib_date[i], unc=True)(centers_117_sim[i])
 
 utheta_0s = np.array([un.ufloat(t, 0.1, tag='angle') for t in theta_0s]) - un.ufloat(-0.09, 0.04, tag='forma')
 m_133 = np.array([(1 - un.umath.cos(un.umath.radians(utheta_0s[i]))) / (1 / centers_133[i] - 1 / 1.33) for i in range(len(utheta_0s))])
 m_117 = np.array([(1 - un.umath.cos(un.umath.radians(utheta_0s[i]))) / (1 / centers_117[i] - 1 / 1.17) for i in range(len(utheta_0s))])
+m_133_sim = np.array([(1 - un.umath.cos(un.umath.radians(utheta_0s[i]))) / (1 / centers_133_sim[i] - 1 / 1.33) for i in range(len(utheta_0s))])
+m_117_sim = np.array([(1 - un.umath.cos(un.umath.radians(utheta_0s[i]))) / (1 / centers_117_sim[i] - 1 / 1.17) for i in range(len(utheta_0s))])
 
 biases = np.array([bias.bias_double(1.33, 1.17, theta_0s[i], calib_date[i]) for i in range(len(theta_0s))])
 
 m_133 -= biases[:,0]
 m_117 -= biases[:,1]
+m_133_sim -= biases[:,0]
+m_117_sim -= biases[:,1]
 
 fig2 = plt.figure('fit_result')
 fig2.clf()
 ax = fig2.add_subplot(111)
 
-ax.errorbar(np.arange(len(theta_0s)) - 0.1, unp.nominal_values(m_133), yerr=unp.std_devs(m_133), fmt='.', label='1.33')
-ax.errorbar(np.arange(len(theta_0s)) + 0.1, unp.nominal_values(m_117), yerr=unp.std_devs(m_117), fmt='.', label='1.17')
+ax.errorbar(np.arange(len(theta_0s)) - 0.05, unp.nominal_values(m_133), yerr=unp.std_devs(m_133), fmt='.', label='1.33')
+ax.errorbar(np.arange(len(theta_0s)) + 0.05, unp.nominal_values(m_117), yerr=unp.std_devs(m_117), fmt='.', label='1.17')
+ax.errorbar(np.arange(len(theta_0s)) - 0.1, unp.nominal_values(m_133_sim), yerr=unp.std_devs(m_133_sim), fmt='.', label='1.33 simp.')
+ax.errorbar(np.arange(len(theta_0s)) + 0.1, unp.nominal_values(m_117_sim), yerr=unp.std_devs(m_117_sim), fmt='.', label='1.17 simp.')
 ax.set_xticks(np.arange(len(theta_0s)))
 ax.set_xticklabels(theta_0s)
 ax.legend(loc=1)
