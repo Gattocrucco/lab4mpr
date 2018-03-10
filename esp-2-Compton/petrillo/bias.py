df6f273566084401b0f2f8a41475f97686d2c2e1
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-10 11:21:36 +0100

    bias con il rapporto di normalizzazioni fissato

--- esp-2-Compton/petrillo/bias.py
+++ esp-2-Compton/petrillo/bias.py
@@ -25,17 +25,19 @@ def bias(energy=1.33, theta_0=10, calib_date='22feb', N=100000):
 def gauss(x, N, mu, sigma):
     return N / (np.sqrt(2 * np.pi) * sigma) * np.exp(-1/2 * (x - mu)**2 / sigma**2)
 
-def fit_fun(x, N1, mu1, sigma1, N2, mu2, sigma2):
-    return gauss(x, N1, mu1, sigma1) + gauss(x, N2, mu2, sigma2)
+def fit_fun(x, N1, mu1, sigma1, f2, mu2, sigma2):
+    return gauss(x, N1, mu1, sigma1) + gauss(x, N1 * f2, mu2, sigma2)
 
-def bias_double(energy1=1.33, energy2=1.17, theta_0=45, calib_date='22feb', N=100000):
+def bias_double(energy1=1.33, energy2=1.17, theta_0=45, calib_date='22feb', N=100000, fixnorm=False):
     p1, _, wp1, _ = mc9.mc_cached(energy1, theta_0=theta_0, seed=64, N=N, beam_center=0, m_e=0.511, date=calib_date)
     p2, _, wp2, _ = mc9.mc_cached(energy2, theta_0=theta_0, seed=128, N=N, beam_center=0, m_e=0.511, date=calib_date)
     
     c, e, dc = empirical.histogram(np.concatenate([p1, p2]), bins=int(np.sqrt(2 * N)), weights=np.concatenate([wp1, wp2]))
     
     p0 = [np.sum(wp1), np.mean(p1), np.std(p1), np.sum(wp2), np.mean(p2), np.std(p2)]
-    out = lab.fit_curve(fit_fun, e[:-1] + (e[1] - e[0]) / 2, c, dy=dc, p0=p0)
+    p0[3] /= p0[0]
+    pfix = [3] if fixnorm else None
+    out = lab.fit_curve(fit_fun, e[:-1] + (e[1] - e[0]) / 2, c, dy=dc, p0=p0, pfix=pfix)
     
     mu_cal1 = calibration.energy_inverse_calibration(date=calib_date, unc=False)(out.upar[1])
     m_fit1 = (1 - np.cos(np.radians(theta_0))) / (1 / mu_cal1 - 1 / energy1)
