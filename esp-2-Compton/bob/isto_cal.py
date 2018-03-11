136f93708abe0212ed954f542435eed154f88250
Gnegne <robertoribatti@gmail.com>
2018-03-11 17:14:43 +0100

    intro, bibliografia e plot calibrazione

--- esp-2-Compton/bob/isto_cal.py
+++ esp-2-Compton/bob/isto_cal.py
@@ -9,11 +9,11 @@ import copy
 
 cartella ="cal/"
 date =["20feb", "22feb","26feb","27feb"]
-elements0= [["cs","Cs137", [0.662], [3700,4000], 5e2],
-           ["am","Am241", [0.0595], [320,380], 1e4],
-           ["na","Na22",  [0.511], [2900,3100],5e2],
-           ["na","Na22",  [1.275], [7100,7400],1e1],
-           ["co","Co60", [1.173,1.333], [6450,7850],1e2]]
+elements0= [["cs","$^{137}$Cs", [0.662], [3700,4000], 5e2, "Cs137"],
+           ["am","$^{241}$Am", [0.0595], [320,380], 1e4, "Am241"],
+           ["na","$^{22}$Na",  [0.511], [2900,3100],5e2, "Na22"],
+           ["na","$^{22}$Na",  [1.275], [7100,7400],1e1,"Na22"],
+           ["co","$^{60}$Co", [1.173,1.333], [6450,7850],1e2,"Co60"]]
 
 #definition______________________________________
 def bar_line(edges, counts, ax=None, **kwargs):
@@ -40,12 +40,15 @@ def fondo(x,m,q):
 file_print=open("cal/cal.txt","w")
 print("#data elemento energia [Mev] media[digit] sigma[digit] err_media[digit] err_sigma[digit] cov(media,sigma)",file=file_print)
         
-              
+
 for data in date:
+    fig = plt.figure('Spettri di calibrazione %s'%(data),figsize=(9, 9), dpi=150)
+    fig.set_tight_layout(True)
+    fig.clf()
+    sub=0
     for b in arange(len(elements0)):
-        # if b != 4 or data != '20feb': continue
-        if data == '20feb' and b != 4:
-            continue
+        if b != 4 and data == '20feb': continue
+        #if data == '20feb': continue
         elements = copy.deepcopy(elements0)
         a = elements[b]
         ref0=loadtxt(cartella+"histo-22feb-"+a[0]+".dat",unpack=True)
@@ -81,8 +84,8 @@ for data in date:
             val=[a[4],(3*sin+dex)/4,(dex-sin)/4,10**2,(sin+3*dex)/4,(dex-sin)/4,0,0]
             popt,pcov=curve_fit(gaus2,_X,_Y,sigma=_dy,p0=val, maxfev=10000)
             val = popt
-            sin = abs(int(popt[1]-1.6*abs(popt[2])))
-            dex = abs(int(popt[4]+1.6*abs(popt[5])))
+            sin = abs(int(popt[1]-1.8*abs(popt[2])))
+            dex = abs(int(popt[4]+1.8*abs(popt[5])))
             _Y = Y[sin:dex]
             _X = arange(dex-sin)+0.5+sin
             _dy=sqrt(_Y)
@@ -93,8 +96,8 @@ for data in date:
             val=[a[4],(sin+dex)/2,(dex-sin)/2,0,0]
             popt,pcov=curve_fit(distr,_X,_Y,sigma=_dy,p0=val, maxfev=10000)
             val = popt
-            sin = abs(int(abs(popt[1])-1.6*abs(popt[2])))
-            dex = abs(int(abs(popt[1])+1.6*abs(popt[2])))
+            sin = abs(int(abs(popt[1])-1.8*abs(popt[2])))
+            dex = abs(int(abs(popt[1])+1.8*abs(popt[2])))
             _Y = Y[sin:dex]
             _X = arange(dex-sin)+0.5+sin 
             _dy=sqrt(_Y)
@@ -106,18 +109,19 @@ for data in date:
         #plot______________________________________________________
         
         #plot su tutto lo spettro
-        figure(data+" "+el+"_spectrum",figsize=(9, 4), dpi=150).set_tight_layout(True)
-        
-        rc("font",size=14)
-        title("Calibrazione %s del %s" %(data, el), size=16)
-        grid(color="black",linestyle=":")
-        minorticks_on()
-        xlabel("energia [digit]")
-        ylabel("conteggi")
         
         #bar(X*fis,Y,width=lbin*fis)
+        z=linspace(sin,dex,1000)
         if(energy!=elements[3][2]):
-            k=32
+            sub = sub+1
+            temp = fig.add_subplot(2,2,sub)
+            #figure(data+" "+el+"_spectrum",figsize=(9, 4), dpi=150).set_tight_layout(True)
+            title(el)
+            temp.grid()
+            #temp.minorticks_on()
+            temp.set_xlabel("energia [digit]")
+            temp.set_ylabel("conteggi")
+            k=8
             n_bin = len(Y)//k
             Y_rebin = np.zeros(n_bin)
             for i in arange(n_bin):
@@ -126,35 +130,39 @@ for data in date:
                     mean += Y[i*k+j]
                 Y_rebin[i] = mean/k
             X_rebin = arange(n_bin+1)*k
-            bar_line(X_rebin, Y_rebin, linewidth=1)
-        
-        z=linspace(sin,dex,1000)
-        if(nome=="co"):
-            plot(z,gaus2(z,*popt),color="red",linewidth=1)
-            #plot(z,fondo(z,popt[6],popt[7]))
+            bar_line(X_rebin, Y_rebin, linewidth=1, color=[0.6]*3,label="dati")
+    
+            if(nome=="co"):
+                temp.plot(z,gaus2(z,*popt),color=[0]*3,linewidth=1,label="fit")
+                #plot(z,fondo(z,popt[6],popt[7]))
+            else:
+                temp.plot(z,distr(z,*popt),color=[0]*3,linewidth=1,label="fit")
+                #plot(z,fondo(z,popt[3],popt[4]), color="blue")
         else:
-            plot(z,distr(z,*popt),color="red",linewidth=1)
-            #plot(z,fondo(z,popt[3],popt[4]), color="blue")
-        savefig("cal/plot/"+data+"_"+el+".pdf")
-        savefig("cal/plot/"+data+"_"+el+".png")
+            temp.plot(z,distr(z,*popt),color=[0]*3,linewidth=1)
+        temp.legend()
+        
+            
+        #savefig("cal/plot/"+data+"_"+el+".pdf")
+        #savefig("cal/plot/"+data+"_"+el+".png")
         
         #zoom sulla gaussiana    
         
-        figure(data+" "+el+"_"+str(energy[0])+"MeV", figsize=(9, 4), dpi=150).set_tight_layout(True)
-        rc("font",size=14)
-        title("Fit fotopicco a "+str(energy)+"MeV nello spettro del "+el, size=16)
-        grid(color="black",linestyle=":")
-        minorticks_on()
-        xlabel("energia [digit]")
-        ylabel("conteggi")
-        
-        errorbar(_X,_Y, _dy, linestyle="", marker=".", markersize=2, color="black",linewidth=1)
-        if(nome=="co"):
-            plot(z,gaus2(z,*popt),color="red",linewidth=2)
-        else:
-            plot(z,distr(z,*popt),color="red",linewidth=2)
-        savefig("cal/plot/"+data+"_"+el+"_"+str(energy[0])+".pdf")
-        savefig("cal/plot/"+data+"_"+el+"_"+str(energy[0])+".png")
+#        figure(data+" "+el+"_"+str(energy[0])+"MeV", figsize=(9, 4), dpi=150).set_tight_layout(True)
+#        rc("font",size=14)
+#        title("Fit fotopicco a "+str(energy)+"MeV nello spettro del "+el, size=16)
+#        grid(color="black",linestyle=":")
+#        minorticks_on()
+#        xlabel("energia [digit]")
+#        ylabel("conteggi")
+#        
+#        errorbar(_X,_Y, _dy, linestyle="", marker=".", markersize=2, color="black",linewidth=1)
+#        if(nome=="co"):
+#            plot(z,gaus2(z,*popt),color="red",linewidth=2)
+#        else:
+#            plot(z,distr(z,*popt),color="red",linewidth=2)
+#        savefig("cal/plot/"+data+"_"+el+"_"+str(energy[0])+".pdf")
+#        savefig("cal/plot/"+data+"_"+el+"_"+str(energy[0])+".png")
           
         # print result_____________________________________________
         if(nome=="co"):
@@ -179,10 +187,10 @@ for data in date:
         print(lab.format_par_cov(popt,pcov),"\n")
         
         if(nome=="co"):
-            print("%s \t %s \t %f \t %f \t %f \t %f \t %f \t %f \t %f" %(data, el, energy[0],popt[1],abs(popt[2]),sqrt(pcov[1][1]),sqrt(pcov[2][2]),pcov[1][2], pcov[1,4]),file=file_print)
-            print("%s \t %s \t %f \t %f \t %f \t %f \t %f \t %f \t %f" %(data, el, energy[1],popt[4],abs(popt[5]),sqrt(pcov[4][4]),sqrt(pcov[5][5]),pcov[4][5], 0),file=file_print)
+            print("%s \t %s \t %f \t %f \t %f \t %f \t %f \t %f \t %f" %(data, a[5], energy[0],popt[1],abs(popt[2]),sqrt(pcov[1][1]),sqrt(pcov[2][2]),pcov[1][2], pcov[1,4]),file=file_print)
+            print("%s \t %s \t %f \t %f \t %f \t %f \t %f \t %f \t %f" %(data, a[5], energy[1],popt[4],abs(popt[5]),sqrt(pcov[4][4]),sqrt(pcov[5][5]),pcov[4][5], 0),file=file_print)
         else:
-            print("%s \t %s \t %f \t %f \t %f \t %f \t %f \t %f \t %f" %(data, el, energy[0],popt[1],abs(popt[2]),sqrt(pcov[1][1]),sqrt(pcov[2][2]),pcov[1][2], 0),file=file_print)
+            print("%s \t %s \t %f \t %f \t %f \t %f \t %f \t %f \t %f" %(data, a[5], energy[0],popt[1],abs(popt[2]),sqrt(pcov[1][1]),sqrt(pcov[2][2]),pcov[1][2], 0),file=file_print)
 
 file_print.close()
     
