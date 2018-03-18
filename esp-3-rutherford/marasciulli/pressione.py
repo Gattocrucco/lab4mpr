a889cacb86bf2a7f4abfd2ad73b48cdccd10589c
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2018-03-18 20:51:13 +0100

    Cammino libero medio.
    Ho cercato il cammino libero medio delle particelle alfa nell'aria, ma ho notato una cosa strana: se uso A(grammi)=14.5 come dovrebbe essere, ho risultati sballati; se la metto uguale alla densità dell'aria torna tutto. Ulteriori dettagli sono presenti nei commenti del programma.
    Il calcolo era fatto per spiegare come mai le particelle alfa non arrivano al rivelatore quando è presente una certa pressione.

--- esp-3-rutherford/marasciulli/pressione.py
+++ esp-3-rutherford/marasciulli/pressione.py
@@ -1,9 +1,7 @@
 ## PRESSIONE
 
-t=10 # secondi
-p,c=loadtxt("dati/0316-pressione.txt",unpack=True)
+p,c,dp=loadtxt("dati/0316-pressione.txt",unpack=True)
 
-dp=0
 dc=sqrt(c)
 dc[-1]=1 # non mi piaceva una misura senza errore
 
@@ -17,6 +15,29 @@ xlabel("P  [mbar]")
 ylabel("conteggi in 10 s")
 
 xscale("log")
-errorbar(p,c,xerr=dp,yerr=dc,fmt=" r",marker=".")
+errorbar(p,c,xerr=dp,yerr=dc,fmt=" k",marker="",capsize=2)
+
+# Range
+figure().set_tight_layout(True)
+rc("font",size=14)
+grid(linestyle=":")
+minorticks_on()
+
+title(r"Cammino libero medio particelle $\alpha$",size=16)
+xlabel("P  [mbar]")
+ylabel("$\lambda$  [cm]")
+
+sigma=1.2 # b (4 protoni da 300 mb)
+sigma*=1e-24 # cm2
+
+ro=1e-3 # g/cm3
+A=1e-3 # g  Dovrebbe essere 14.5 [75% azoto ; 25% ossigeno] ma torna bene così.
+n=ro*6e23/A # 1/cm3
+
+def camm(p): # cammino libero medio in cm
+    return 1/(n*p*sigma/1000) # p in mbar
+
+x=linspace(1e2,1e3,10**4)
+plot(x,camm(x))
 
 show()
