e5f92021b55db9bcdf02fa1936e24af7d11a25c6
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-19 22:12:42 +0100

    analizzata soglia 19 marzo e corretta 15 marzo
    In quella del 15 marzo mi ero scordato di tenere conto del tempo di acquisizione.

--- esp-3-rutherford/petrillo/soglia.py
+++ esp-3-rutherford/petrillo/soglia.py
@@ -1,24 +1,50 @@
 import tagload
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties as un
+from uncertainties import unumpy as unp
+import lab4
 
-data, tags = tagload.tagload('../dati/0315-soglia.txt')
-
-sorted_thresh = tags['soglia']
-thresh = np.unique(tags['soglia'])
-counts = np.empty(thresh.shape)
-
-for i in range(len(thresh)):
-    counts[i] = np.sum(data[sorted_thresh == thresh[i]])
+labels = ['dati 15 marzo', 'dati 19 marzo']
+colors = ['lightgray'    , 'black'        ]
+fmts   = ['^'            , '.'            ]
 
 fig = plt.figure('soglia')
 fig.clf()
 fig.set_tight_layout(True)
 
 ax = fig.add_subplot(111)
-ax.errorbar(thresh, counts, yerr=np.sqrt(counts), fmt='.k')
-ax.set_ylabel('conteggio')
+
+for label, color, fmt in zip(labels, colors, fmts):
+    # dati 15 marzo
+    if '15' in label:
+        data, tags = tagload.tagload('../dati/0315-soglia.txt')
+
+        sorted_thresh = tags['soglia']
+        thresh = np.unique(tags['soglia'])
+        counts = np.empty(thresh.shape)
+        times = np.zeros(thresh.shape, dtype=object)
+
+        for i in range(len(thresh)):
+            this_data = data[sorted_thresh == thresh[i]]
+            counts[i] = np.sum(this_data)
+            for j in range(len(this_data)):
+                times[i] += un.ufloat(10000, 0.5) * 1e-3
+        counts = unp.uarray(counts, np.sqrt(counts))
+        rates = counts / times
+
+    # dati 19 marzo
+    elif '19' in label:
+        thresh, counts, clock = np.loadtxt('../dati/0319-soglia.txt', unpack=True)
+        counts = unp.uarray(counts, np.sqrt(counts))
+        times = unp.uarray(clock, 0.5) * 1e-3
+        rates = counts / times
+
+    lab4.errorbar(thresh, rates, ax=ax, fmt=fmt, color=color, label=label, markersize=3)
+
+ax.set_ylabel('rate [s$^{-1}$]')
 ax.set_xlabel('soglia discriminatore')
 ax.grid(linestyle=':')
+ax.legend(loc='best', fontsize='small')
 
 fig.show()
