e56fe973e98558303c980d7b69cc1737edc55e38
Giacomo Petrillo <info@giacomopetrillo.com>
2018-03-19 23:00:03 +0100

    ritardo discriminatore

---
+++ esp-3-rutherford/petrillo/ritardo.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+thresh, ant, unc_ant = np.loadtxt('../dati/0319-ritardo.txt', unpack=True)
+
+fig = plt.figure('ritardo')
+fig.clf()
+fig.set_tight_layout(True)
+ax = fig.add_subplot(111)
+
+ax.errorbar(thresh, ant, yerr=unc_ant, fmt='.k')
+
+ax.set_xlabel('soglia discriminatore [V]')
+ax.set_ylabel('ritardo segnale discriminato [ns]')
+ax.grid(linestyle=':')
+
+fig.show()
