8fe367cb244ff7b2f8821fd997a40a89e536fa03
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2018-04-16 18:49:16 +0200

    Fit sezione d'urto
    Programma funzionante con l'alluminio

--- esp-3-rutherford/marasciulli/sezione/rateang.py
+++ esp-3-rutherford/marasciulli/sezione/rateang.py
@@ -6,10 +6,18 @@ import lab
 from uncertainties.unumpy import nominal_values as nom
 from uncertainties.unumpy import std_devs as err
 from scipy.special import chdtrc
+from uncertainties import ufloat as uf
+
+'''
+parametri oro 0.2um: A=7.5(4)e-5 , ang0=1.6(4)°
+parametri oro 5um:
+parametri allumnio 8um: A=3.5(3)e-7 ang0=0.8(4)°
+'''
 
 files = [
-    '0322-oro0.2coll1.txt',
-    '0322-oro0.2coll5.txt',
+'0327-all8coll1.txt',
+'0412-all8coll5.txt',
+'0413-all8coll5.txt'
 ]
 
 fig = plt.figure('rateang')
@@ -18,10 +26,8 @@ fig.set_tight_layout(True)
 
 ax1 = fig.add_subplot(111)
 
-# array per il concatenate
-
-angoli=np.array([])
-tassi=np.array([])
+atot=[] # set di angoli
+rtot=[] # set di rate
 
 for file in files:
     ang, count, clock = np.loadtxt('../dati/{}'.format(file), unpack=True)
@@ -30,58 +36,65 @@ for file in files:
     time = unp.uarray(clock, 0.5) * 1e-3
     rate = count / time
     ang = unp.uarray(ang, 1)
-    '''
-    sto supponendo che nel passaggio tra collimatori l'ultimo angolo 
-    del primo file sia il primo angolo del secondo
-    '''
+
+    atot.append(ang)
+    rtot.append(rate)
+    
     if 'coll1' in file:
-        ultimo=rate[-1]
-    if 'coll5' in file:
-        rapp=rate[0]/ultimo
-        rate/=rapp
-    
-    angoli=np.append(angoli,ang)
-    tassi=np.append(tassi,rate)
-    
-# selezione
-
-fuori=[-10,20]  # estremi compresi
-dentro=[-70,70]  # estremi inclusi
-angf=np.array([]) # f sta per fit
-ratef=np.array([]) 
-
-for i in range(len(angoli)):
-    if dentro[0]<=angoli[i]<=fuori[0] or fuori[1]<=angoli[i]<=dentro[1]:
-        angf=np.append(angf,angoli[i])
-        ratef=np.append(ratef,tassi[i])
+        a='blue'
     else:
-        pass
+        a='red'
+    lab4.errorbar(ang,rate,capsize=2,label="%s"%file,linestyle='',color=a)
 
-# fit semplice
+# selezioni
+w=np.concatenate((atot[1:]))
+rr=np.concatenate((rtot[1:]))
 
-def rute(teta,A,c):
-    return A/(np.sin((teta-c)/2))**4  
+fuori=[-5,10] # estremi inclusi
 
-p0=[1,np.radians(3.5)]
-# ricordatevi gli angoli in radianti
-fit=lab.fit_curve(rute,np.radians(nom(angf)),nom(ratef),dx=np.radians(err(angf)),dy=err(ratef),p0=p0,print_info=1)
+# selezione per il coll1
+y1=[]
+for l in range(len(atot[0])):
+    if fuori[0]<atot[0][l]<fuori[1]:
+        y1.append(l)
+atot[0]=np.delete(atot[0],y1)
+rtot[0]=np.delete(rtot[0],y1)
 
-dof=len(angf)-len(fit.par)
-print("chi quadro=",fit.chisq,"+-",np.sqrt(2*dof),"  dof=",dof,"\n")
-print("zero vero= {:1uP}°".format(unp.degrees(fit.upar[1])))
+# selezione per il coll5
+y5=[]
+for k in range(len(w)):
+    if fuori[0]<w[k]<fuori[1]:
+        y5.append(k)
+w=np.delete(w,y5)
+rr=np.delete(rr,y5)
+
+w=np.delete(w,-1) # correzione fatta a mano ATTENZIONE
+rr=np.delete(rr,-1)
+
+# fit
+# mi serve una selezione con gli angoli
+
+def fitfun(teta,A,tc):
+    return A/np.sin((teta-tc)/2)**4
+
+# fit1 e fit5 prendono il nome dai collimatori
+val1=[1e-6,np.radians(2)]
+fit1=lab.fit_curve(fitfun,np.radians(nom(atot[0])),nom(rtot[0]),dx=err(unp.radians(atot[0])),dy=err(rtot[0]),p0=val1,print_info=1)
+
+val2=[1e-5,np.radians(2)]
+fit2=lab.fit_curve(fitfun,np.radians(nom(w)),nom(rr),dx=err(unp.radians(w)),dy=err(rr),p0=val2,print_info=1)
 
 # grafico    
 
 ax1.set_xlabel('angolo [°]')
 ax1.set_ylabel('rate [s$^{-1}$]')
 ax1.grid(linestyle=':')
+ax1.set_xlim(min(nom(w)),max(nom(w)))
 
-lab4.errorbar(angoli,tassi,capsize=2,marker=".",linestyle="")  # non posso fare ax1.lab4.errorbar
+z1=np.linspace(min(nom(atot[0])),max(nom(atot[0])),1000)
+z2=np.linspace(min(nom(w)),max(nom(w)),1000)
+ax1.plot(z1,fitfun(np.radians(z1),*fit1.par),'b',scaley=False)
+ax1.plot(z2,fitfun(np.radians(z2),*fit2.par),'r',scaley=False)
 
-z1=np.linspace(dentro[0],fuori[0],1000)
-ax1.plot(z1,rute(np.radians(z1),*fit.par),color="red")
-
-z2=np.linspace(fuori[1],dentro[1],1000)
-ax1.plot(z2,rute(np.radians(z2),*fit.par),color="red")
-
+plt.legend()
 fig.show()
