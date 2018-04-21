dc04ee4d1f7cc7ee8700570b4ed6b99f87bb8a4a
Giacomo Petrillo <info@giacomopetrillo.com>
2018-04-22 01:44:14 +0200

    montecarlo funzionante
    Ma è da ottimizzare un po'.

--- esp-3-rutherford/petrillo/dedx.py
+++ esp-3-rutherford/petrillo/dedx.py
@@ -3,10 +3,13 @@
 import numpy as np
 import lab4
 
-T, dedx = np.loadtxt('../bob/stopping_power_au.txt', unpack=True)
-rho = 19.320 # densità dell'oro
+T, dedx_data = np.loadtxt('../bob/stopping_power_au.txt', unpack=True)
 
-f = lab4.interp(T, dedx * rho)
+rho_au = 19.320 # densità dell'oro
+rho_al = 2.699
+
+dedx = lab4.interp(T, dedx_data)
+dedx_min = np.min(T)
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
@@ -15,13 +18,13 @@ if __name__ == '__main__':
     fig.set_tight_layout(True)
 
     ax = fig.add_subplot(111)
-    ax.plot(T, dedx * rho, '.k')
+    ax.plot(T, dedx_data * rho_au / 10000, '.k')
     x = np.linspace(np.min(T), np.max(T), 10000)
-    ax.plot(x, [f(x) for x in x], '-k')
+    ax.plot(x, [dedx(x) * rho_au / 10000 for x in x], '-k')
 
     ax.set_xlabel('energia [MeV]')
-    ax.set_ylabel('dE/dx [MeV / cm]')
-    ax.set_title('Perdita di energia particelle $\\alpha$')
+    ax.set_ylabel('dE/dx [MeV $\\mu$m$^{-1}$]')
+    ax.set_title('Perdita di energia particelle $\\alpha$ nell\'oro')
     ax.grid(linestyle=':')
 
     fig.show()
