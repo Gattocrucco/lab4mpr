2a30b7e386f13e2aae3e4612738468d58d603136
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2018-04-21 13:30:58 +0200

    Dati e programmi

--- esp-3-rutherford/marasciulli/sezione/sigma.py
+++ esp-3-rutherford/marasciulli/sezione/sigma.py
@@ -10,12 +10,12 @@ from scipy.special import chdtrc
 from uncertainties import ufloat as uf
 from scipy.integrate import quad
 
-par_all=(uf(3.529676144312048e-05 , 2.5886646789266666e-06) , uf(0.030456451504531253 , 0.008192846854198087))  # coll5
+par_all=(uf(3.7e-05 , 0.3e-5) , uf(0.043 , 0.007))  # coll5
 par_oro0_2=(uf(0.00012538143518207068 , 1.078666576167907e-05), uf(0.05670765051091632 , 0.00665115129475818))  # coll1
 
 def rute(teta,A,tc):
     return A/np.sin((teta-tc)/2)**4
  
-par_oro0_2=tuple(nom(par_oro0_2))
-sigma=quad(rute,np.radians(15),np.pi,args=par_oro0_2)
+par_all=tuple(nom(par_all))
+sigma=quad(rute,np.radians(3),np.pi,args=par_all)
 print("sezione d'urto=",sigma[0])
