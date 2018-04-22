a352f3637bba764617c72787e36ebd15daa63e9e
Giacomo Petrillo <info@giacomopetrillo.com>
2018-04-22 15:02:48 +0200

    aggiunta stopping power al
    È diversa da quella dell'oro, adesso le particelle simulate perdono più energia di prima nell'alluminio.

--- esp-3-rutherford/petrillo/dedx.py
+++ esp-3-rutherford/petrillo/dedx.py
@@ -3,13 +3,20 @@
 import numpy as np
 import lab4
 
-T, dedx_data = np.loadtxt('../bob/stopping_power_au.txt', unpack=True)
+T_au, dedx_au_data = np.loadtxt('../bob/stopping_power_au.txt', unpack=True)
+T_al, _, _, dedx_al_data = np.loadtxt('../bob/stopping_power_al.txt', unpack=True)
 
-rho_au = 19.320 # densità dell'oro
-rho_al = 2.699
+rho_au = 19.320 # g/cm^3
+rho_al = 2.699 # g/cm^3
 
-dedx = lab4.interp(T, dedx_data)
-dedx_min = np.min(T)
+dedx_au = lab4.interp(T_au, dedx_au_data)
+dedx_al = lab4.interp(T_al, dedx_al_data)
+dedx_min = max(np.min(T_au), np.min(T_al))
+
+dedx = {
+    13: dedx_al,
+    79: dedx_au
+}
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
@@ -18,13 +25,17 @@ if __name__ == '__main__':
     fig.set_tight_layout(True)
 
     ax = fig.add_subplot(111)
-    ax.plot(T, dedx_data * rho_au / 10000, '.k')
-    x = np.linspace(np.min(T), np.max(T), 10000)
-    ax.plot(x, [dedx(x) * rho_au / 10000 for x in x], '-k')
+    ax.plot(T_au, dedx_au_data, '.', label='Au data')
+    ax.plot(T_al, dedx_al_data, '.', label='Al data')
+    x = np.linspace(np.min(T_au), np.max(T_au), 10000)
+    ax.plot(x, [dedx_au(x) for x in x], '-k')
+    ax.plot(x, [dedx_al(x) for x in x], '-k')
 
     ax.set_xlabel('energia [MeV]')
-    ax.set_ylabel('dE/dx [MeV $\\mu$m$^{-1}$]')
-    ax.set_title('Perdita di energia particelle $\\alpha$ nell\'oro')
+    ax.set_ylabel('dE/dx [MeV g$^{-1}$ cm$^{2}$]')
+    ax.set_title('Perdita di energia particelle $\\alpha$')
     ax.grid(linestyle=':')
+    ax.set_xlim(0, 10)
+    ax.legend(loc=1)
 
     fig.show()
