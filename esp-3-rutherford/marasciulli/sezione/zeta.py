138280a0ea3bd2389b584895dee88f1982e839ea
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2018-04-23 11:42:43 +0200

    Z alluminio

--- esp-3-rutherford/marasciulli/sezione/zeta.py
+++ esp-3-rutherford/marasciulli/sezione/zeta.py
@@ -9,16 +9,7 @@ from uncertainties.unumpy import std_devs as err
 from scipy.special import chdtrc
 from uncertainties import ufloat as uf
 
-# preparazione dati
-# per far tornare tutto (almeno adesso) ho commentato i dati a doppio
 
-ang_oro,rate_oro,drate_oro=np.loadtxt('oro0_2.txt',unpack=True)  # cambiare da qui lo spessore
-ang_all,rate_all,drate_all=np.loadtxt('alluminio.txt',unpack=True)
-
-ang_oro=unp.uarray(ang_oro,1)
-rate_oro=unp.uarray(rate_oro,drate_oro)
-ang_all=unp.uarray(ang_all,1)
-rate_all=unp.uarray(rate_all,drate_all)
 
 l_au=5
 l_all=8
@@ -28,30 +19,26 @@ n_all=2.699/27
 
 z_au=79
 
-# calcoli
+# scopo del gioco: commentare quello che non serve
 
-ang_au=set(nom(ang_oro))
-ang_al=set(nom(ang_all))
-scelti=set.intersection(ang_au,ang_al)
-scelti=list(scelti)
+# parametri alluminio
+# coll1
+B_al=uf(5.7,0.1)*1e-6
+# coll5
+#B_al=uf(1.92,0.14)*1e-5
 
-z_all=np.array([])
-for i in range(len(scelti)):
-    if (scelti[i] in nom(ang_oro)) and (scelti[i] in nom(ang_all)):
-        elemento=z_au*unp.sqrt( n_au/n_all * l_au/l_all * rate_all[nom(ang_all)==scelti[i]]/rate_oro[nom(ang_oro)==scelti[i]] )
-        z_all=np.append(z_all,elemento)
+# parametri oro 3 um
+# coll1
+#B_au=uf(1.2,0.1)*1e-4
+# coll5
+#B_au=uf(5.26,0.22)*1e-4
 
-# grafico
+# parametri oro 5 um
+# coll1
+B_au=uf(1.2,0.1)*1e-4
+# coll5
+#B_al=uf(6.8,0.3)*1e-4
 
-plt.figure().set_tight_layout(True)
-plt.rc('font',size=16)
-plt.grid(linestyle=":")
-plt.minorticks_on()
+z_al=z_au*unp.sqrt( B_al/B_au * n_au/n_all * l_au/l_all )
 
-plt.title("Z dell'alluminio",size=18)
-plt.xlabel("angolo  [°]")
-plt.ylabel("Z")
-
-plt.errorbar(scelti,nom(z_all),xerr=1,yerr=err(z_all),fmt='.k')
-
-plt.show()
+print("Z_alluminio={:1uP}".format(z_al))
