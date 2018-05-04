7c1fed773ad148deb721d764df85d20e05d1a592
Giacomo Petrillo <info@giacomopetrillo.com>
2018-05-05 00:30:27 +0200

    DAQ
    Ho già tolto gli eventi appesi per sbaglio a 0504_cal_na_ch1.txt.

--- esp-4-positroni/DAQ/histo.py
+++ esp-4-positroni/DAQ/histo.py
@@ -12,29 +12,69 @@ out2=ch2[(tr2>500)]
 out3=ch3[(tr3>500)]
 out4=ch1[(tr1>500) & (c2>500)]
 out5=ch2[(tr2>500) & (c2>500)]
-out6=ch3[(tr3>500) & (c2>500)]
+
+tre1=ch1[(tr1>500) & (c3>500)]
+tre2=ch2[(tr2>500) & (c3>500)]
+out6=ch3[(tr3>500) & (c3>500)]
 
 tuttivecchio=arange(0,max(tr1))
 tutti=arange(0,1200//8)*8
 
 figure(1)
 
-#hist(out1,bins=tutti,label="ch1 %d"%len(out1),histtype="step")
-#hist(out2,bins=tutti,label="ch2 %d"%len(out2),histtype="step")
-#hist(out3,bins=tutti,label="ch3 %d"%len(out3),histtype="step")
-hist(out4,bins=tutti,label="ch1 c2 %d"%len(out4),histtype="step")
-hist(out5,bins=tutti,label="ch2 c2 %d"%len(out5),histtype="step")
-#hist(out6,bins=tutti,label="ch3 c2 %d"%len(out6),histtype="step")
+hist(out1,bins=tutti,label="ch1 n=%d"%len(out1),histtype="step")
+hist(out2,bins=tutti,label="ch2 n=%d"%len(out2),histtype="step")
+hist(out3,bins=tutti,label="ch3 n=%d"%len(out3),histtype="step")
 legend(loc=0)
 
 figure(2)
 
+due3=ch3[(tr3>500) & (c2>500)]
+
+hist(out4,bins=tutti,label="ch1 c2 n=%d"%len(out4),histtype="step")
+hist(out5,bins=tutti,label="ch2 c2 n=%d"%len(out5),histtype="step")
+#hist(due3,bins=tutti,label="ch3 c2 n=%d"%len(due3),histtype="step")
+legend(loc=0)
+
+figure(3)
+
+hist(tre1,bins=tutti,label="ch1 c3 n=%d"%len(tre1),histtype="step")
+hist(tre2,bins=tutti,label="ch2 c3 n=%d"%len(tre2),histtype="step")
+hist(out6,bins=tutti,label="ch3 c3 n=%d"%len(out6),histtype="step")
+
+legend(loc=0)
+
+
+figure(4)
+
 sc1=ch1[c2>500]
 sc2=ch2[c2>500]
 
-plot(sc1,sc2,linestyle='',marker='.',markersize=2)
+title("scatter plot coincidenze a 2")
+plot(sc1,sc2,linestyle='',marker='.',markersize=1)
 xlabel("ch1")
 ylabel("ch2")
 
-#legend(loc=0)
+figure(5)
+
+sc33=ch3[c3>500]
+sc13=ch1[c3>500]
+sc23=ch2[c3>500]
+
+plot(sc13,sc23,linestyle='',marker='.',markersize=1)
+xlabel("ch1")
+ylabel('ch2')
+
+figure(6)
+
+plot(sc23,sc33,linestyle='',marker='.',markersize=1)
+xlabel("ch2")
+ylabel('ch3')
+
+figure(7)
+
+plot(sc13,sc33,linestyle='',marker='.',markersize=1)
+xlabel("ch1")
+ylabel('ch3')
+
 show()
--- esp-4-positroni/petrillo/histo2d.py
+++ esp-4-positroni/petrillo/histo2d.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-def histo2d(chx, chy):
+def histo2d(chx, chy, ax=None, **kw):
     bins = np.arange(0, 1200 // 8) * 8
     H, _, _ = np.histogram2d(chx, chy, bins=[bins, bins])
+    
+    if not (ax is None):
+        ax.imshow(-np.log(1 + H), cmap='gray', extent=[np.min(bins), np.max(bins)] * 2, **kw)
+    
     return H, bins
 
 if __name__ == '__main__':
@@ -18,8 +22,7 @@ if __name__ == '__main__':
     
     ax = fig.add_subplot(111)
     
-    H, bins = histo2d(ch1[c2 > 500], ch2[c2 > 500])
-    ax.imshow(-np.log(1+H), cmap='gray')
+    H, bins = histo2d(ch1[c2 > 500], ch2[c2 > 500], ax=ax)
     ax.set_xlabel('ch1 c2')
     ax.set_ylabel('ch2 c2')
     
