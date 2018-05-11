ef72e91795a84118fb3068272781e9445a616484
Giacomo Petrillo <info@giacomopetrillo.com>
2018-05-11 21:19:05 +0200

    zoom su c3 3D
    Codice di Bob.

--- esp-4-positroni/petrillo/histo3d.py
+++ esp-4-positroni/petrillo/histo3d.py
@@ -1,4 +1,5 @@
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -18,5 +19,11 @@ ax.scatter(ch1[c3], ch2[c3], ch3[c3], color='black', marker='o')
 ax.set_xlabel('ch1')
 ax.set_ylabel('ch2')
 ax.set_zlabel('ch3')
+
+ax.set_xlim(220, 250)
+ax.set_ylim(220, 250)
+ax.set_zlim(200,300)
+
+
 
 fig.show()
