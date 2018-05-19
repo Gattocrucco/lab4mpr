24c002c640996b4e68ebdfd9979ca4931bb9eafb
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2018-05-19 13:32:19 +0200

    Stabil incompleto
    Da finire dopo pranzo

--- esp-4-positroni/marasciulli/slice/stabil.py
+++ esp-4-positroni/marasciulli/slice/stabil.py
@@ -2,25 +2,27 @@
 from pylab import *
 import lab4
 import lab
+from fit_peak import fit_peak
+import gvar
 
 
 file="0503_stab"
-cartella="../../DAQ/"
+cartella="../DAQ/"
 ch1,ch2,ch3,tr1,tr2,tr3,c2,c3,ts=lab4.loadtxt(cartella+file+".txt",unpack=True,usecols=(0,1,2,4,5,6,8,9,12))
 
-pezzi=5
-chname='ch1' # sono ammessi: ch1,ch2,ch3 
+pezzi=4
+chname='ch2' # sono ammessi: ch1,ch2,ch3 
 
 # dati di calibrazione
-out1=ch1[tr1>500]
-out2=ch2[tr2>500]
-out3=ch3[tr3>500]
+out1=ch1[(ch1>150) & (ch2<150)]
+out2=ch2[(ch1<150) & (ch2>150)]
+out3=ch3[ch3>150]
 
-ts1=ts[tr1>500]
-ts2=ts[tr2>500]
-ts3=ts[tr3>500]
+ts1=ts[(ch1>150) & (ch2<150)]
+ts2=ts[(ch1<150) & (ch2>150)]
+ts3=ts[ch3>150]
 
-figure('tempo')
+figure('tempo').set_tight_layout(True)
 clf()
 
 title("Scalibrazione del %s in %s"%(chname,file))
@@ -36,6 +38,9 @@ else:
     canale=out3; cant=ts3
     
 outputs=[]
+beta=array([])
+neon=array([])
+tempi=array([])
 
 for j in range(pezzi):
     slice=canale[int((j/pezzi)*len(canale)) :int(((j+1)/pezzi)*len(canale))]
@@ -45,46 +50,60 @@ for j in range(pezzi):
     else:
         tempo=(max(cant)-ts[0])/3600
         
+    tempi=append(tempi,tempo)
+        
     counts,edges=histogram(slice,bins=arange(0,1200//8)*8)
     line, = lab4.bar(edges, counts, label='$\Delta t$=%.1f ore'%tempo)
     color=line.get_color()
     legend()
     
+    X=edges[1:]-edges[:1]/2
     
     # fit
     def gauss(x, peak, mean, sigma):
         return peak * np.exp(-(x - mean) ** 2 / sigma ** 2)
     
-    
-    x = (edges[1:] + edges[:-1]) / 2
-    
-    # picchi
-    
     for i in range(2):
         if i==0:
-            y=x[x<500]
-            count=counts[x<500]
+            bordi=edges[edges<=500]
+            count=counts[X<500]
         else:
-            y=x[x>500]
-            count=counts[x>500]
-    
-        p0 = [1] * 3
-        argmax = np.argmax(count)
-        # initial parameters
-        p0[0] = count[argmax] # peak
-        p0[1] = y[argmax] # mean
-        p0[2] = 50 # sigma
-        cut = (count > count[argmax] / 3) & (y>350)
-        if np.sum(cut) > 1:
-            out = lab.fit_curve(gauss, y[cut], count[cut], p0=p0, dy=np.sqrt(count)[cut], print_info=1)
-            outputs.append(out)
-        else:
-            outputs.append(None)
+           bordi=edges[edges>=496]
+           count=counts[X>500]
         
-        # plot
-        if not outputs[-1] is None:
-            xspace = np.linspace(np.min(y[cut]), np.max(y[cut]), 1000)
-            plot(xspace, gauss(xspace, *out.par), '--',color=color)
+        argmax = np.argmax(count)
+        cut = (bordi[argmax]-40,bordi[argmax]+40)
+        ordinata=gvar.gvar(count,sqrt(count))
+        
+        if i==0:
+            outdict,indict = fit_peak(bordi,ordinata,bkg='exp',npeaks=1,ax=gca(),cut=cut,plot_kw={'color':color})
+        else:
+            outdict,indict = fit_peak(bordi,ordinata,bkg=None,npeaks=1,ax=gca(),cut=cut,plot_kw={'color':color})
     
+
+'''
+figure('grafico').set_tight_layout(True)
+clf()
+
+title("Scalibrazione del %s in %s"%(chname,file))
+ylabel("valore ADC [digit]")
+xlabel("tempo  [ore]")
+grid(linestyle=':')
+minorticks_on()
+
+subplot(211)
+lab4.errorbar(tempi,beta,fmt='.r',label='Annichilazione')
+legend(loc=0)
+grid(linestyle=':')
+minorticks_on()
+
+subplot(212)
+lab4.errorbar(tempi,neon,fmt='.g',label='Neon')
+
+grid(linestyle=':')
+minorticks_on()
+
+legend(loc='lower right')
 minorticks_on()
 show()
+'''
