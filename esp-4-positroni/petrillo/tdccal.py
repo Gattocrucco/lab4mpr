e2932ad349487eb20ad8d896bfb4da8a36fd8c70
Andrea-Marasciulli <33788972+Andrea-Marasciulli@users.noreply.github.com>
2018-05-20 12:24:27 +0200

    tdccal.py corretto

--- esp-4-positroni/petrillo/tdccal.py
+++ esp-4-positroni/petrillo/tdccal.py
@@ -12,6 +12,7 @@ ax = fig.add_subplot(111)
 files = glob.glob('../DAQ/0517_*ns_provatdc.txt')
 nominal, tdc = np.empty((2, len(files)))
 for i in range(len(files)):
+    files[i] = files[i].replace("\\", '/')
     samples, = lab4.loadtxt(files[i], unpack=True, usecols=(0,), dtype=int)
     nominal[i], = scanf.scanf('../DAQ/0517_%dns_provatdc.txt', s=files[i])
     tdc[i] = np.mean(samples)
